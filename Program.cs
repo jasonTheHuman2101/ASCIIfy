86741e231e9e14d52927c3cbd63acf5d6cb73ce9
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2022-09-22 15:51:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace ASCIIfy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0) { Console.WriteLine("No file path to image provided"); }
+            else
+            {
+                bool passed = false;
+                try
+                {
+                    File.ReadAllBytes(args[0]);
+                    passed = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid File");
+                }
+                if (passed) { RenderImage(args[0]); }
+            }
+        }
+
+        static void RenderImage(string path)
+        {
+            //Open image
+            Bitmap b = new Bitmap(path);
+            int width = b.Width;
+            int height = b.Height;
+        }
+    }
+}
